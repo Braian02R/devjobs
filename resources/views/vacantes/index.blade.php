36efdf607aa928ce03396deae824b066cdae170c
Braian <bf02rojas.edu@gmail.com>
2025-01-24 18:28:01 -0300

    Creacion del panel de mostrar vacantes del administrador con livewire y publish de pagination

--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -15,11 +15,8 @@
                 </div>
             @endif
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Mis vacantes") }}
-                </div>
-            </div>
+            <livewire:mostrar-vacantes />
+            
         </div>
     </div>
 </x-app-layout>
